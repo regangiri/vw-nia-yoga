47c4fe19a96bc8ca6025d696af5183494e39c39d
regangiri <regangirikaruna@gmail.com>
2024-09-14 13:39:37 +0700

    fix(images-gallery): fix image not 3 columned on iphone

--- components/ImageGallery/ImageGallery.tsx
+++ components/ImageGallery/ImageGallery.tsx
@@ -20,13 +20,13 @@ export default function ImageGallery() {
   return (
     <div
       ref={ref}
-      className="w-full bg-palewhite pb-6 overlflow-hidden text-md text-center flex flex-col items-center"
+      className="w-full bg-palewhite pb-6 overflow-hidden text-md text-center flex flex-col items-center"
     >
       <motion.h1 className="text-3xl xxs:text-4xl sm:text-5xl text-primary text-center py-6 font-baskervville">
         Our Cherished Moments
       </motion.h1>
 
-      <motion.div className="images-container relative w-full gap-1 md:gap-3 lg:gap-2 columns-3">
+      <motion.div className="images-container relative w-full gap-1 md:gap-3 lg:gap-2 !columns-3">
         {images.map((image, index) => {
           return (
             <Image
